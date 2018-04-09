21eab44b26ef04c1ea251f6e83caa75a31e0ecf0
Kamil Cieślik <kamil.cieslik@protonmail.com>
2018-04-09 23:24:39 +0200

    Adding FXML loading constructor to PlayerClientBean class.

--- src/main/java/player_fx_bean/PlayerClientBean.java
+++ src/main/java/player_fx_bean/PlayerClientBean.java
@@ -1,15 +1,28 @@
 package player_fx_bean;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PlayerClientBean extends VBox implements Serializable, Initializable {
+
+    public PlayerClientBean() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/player_client_bean.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
 
     @FXML
     private Label labelPlayerNick, labelCaptainNick, labelNumberOfPlayers, labelNumberOfPoints, labelPanelName,
