6307346072d58bf7e04351e9bd6b70644f87ef30
Kamil Cieślik <kamil.cieslik@protonmail.com>
2018-04-09 21:50:40 +0200

    Adding player_client_bean.fxml file.

--- src/main/java/player_fx_bean/PlayerClientBean.java
+++ src/main/java/player_fx_bean/PlayerClientBean.java
@@ -1,9 +1,16 @@
 package player_fx_bean;
 
+import javafx.fxml.Initializable;
 import javafx.scene.layout.VBox;
 
 import java.io.Serializable;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PlayerClientBean extends VBox implements Serializable {
+public class PlayerClientBean extends VBox implements Serializable, Initializable {
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
